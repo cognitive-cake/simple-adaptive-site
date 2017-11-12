2a388cb84411c95073084b4929dc92b43e438322
Nick-ch <nd.chernikov@gmail.com>
2017-11-12 14:18:14 +0300

    begin configuration

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+/* eslint-disable */
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var server = require('browser-sync').create();
+var mqpacker = require('css-mqpacker');
+var minify = require('gulp-csso');
+var rename = require('gulp-rename');
+var run = require('run-sequence');
+var del = require('del');
+var pug = require('gulp-pug');
+var htmlbeautify = require('gulp-html-beautify');
+
+gulp.task('style-project', function () {
+  gulp.src('sass/style.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        'last 1 version',
+        'last 2 Chrome versions',
+        'last 2 Firefox versions',
+        'last 2 Opera versions',
+        'last 2 Edge versions'
+      ]}),
+      mqpacker({
+        sort: true
+      })
+    ]))
+    .pipe(gulp.dest('css'))
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('css'))
+    .pipe(server.stream());
+});
+
+gulp.task('pug', function buildHTML() {
+  return gulp.src('pug/*.pug')
+    .pipe(pug())
+    .pipe(htmlbeautify())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('serve', function () {
+  server.init({
+    server: '.',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch('sass/**/*.{scss,sass}', ['style-project']);
+  gulp.watch('*.html').on('change', server.reload);
+});
